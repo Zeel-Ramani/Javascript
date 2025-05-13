54f5158f4b6a8d4c000b79a9abf74c6ca1e6a036
Zeel-Ramani <zeelrramani@gmail.com>
2025-05-13 20:32:32 +0530

    corona cases

---
+++ Import-Export/js/index.js
@@ -0,0 +1,6 @@
+// Import, Export & Spread Operators
+import { add, subtract } from './math.js';
+import { PI } from './constants.js';
+console.log(add(2, 3));
+console.log(subtract(2, 3));
+console.log(PI);
